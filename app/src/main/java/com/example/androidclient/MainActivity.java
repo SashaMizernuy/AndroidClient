8e83b1a4eb2097b8b2bc71d04478f952e044ab80
SashaMizernuy <sasha.mizernuy@gmail.com>
2019-09-02 09:34:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidclient/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.androidclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textResponse;
+    EditText editTextAddress, editTextPort;
+    EditText LatLon;
+    Button buttonConnect, buttonClear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextAddress = (EditText) findViewById(R.id.address);
+        editTextPort = (EditText) findViewById(R.id.port);
+        LatLon = (EditText) findViewById(R.id.lat);
+        buttonConnect = (Button) findViewById(R.id.connect);
+        buttonClear = (Button) findViewById(R.id.clear);
+        textResponse = (TextView) findViewById(R.id.response);
+
+        buttonConnect.setOnClickListener(buttonConnectOnClickListener);
+
+        buttonClear.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                textResponse.setText("");
+            }
+        });
+    }
+
+    OnClickListener buttonConnectOnClickListener =
+            new OnClickListener(){
+
+                @Override
+                public void onClick(View arg0) {
+
+                    MyClientTask myClientTask = new MyClientTask(
+                            editTextAddress.getText().toString(),
+                            Integer.parseInt(editTextPort.getText().toString()),LatLon.getText().toString());
+                    myClientTask.execute();
+                }};
+
+    public class MyClientTask extends AsyncTask<Void, Void, Void> {
+
+        String dstAddress;
+        int dstPort;
+        String response = "";
+        String lat;
+
+
+        MyClientTask(String addr, int port, String s){
+            dstAddress = addr;
+            dstPort = port;
+            lat=s;
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            Socket socket = null;
+
+
+            try {
+                socket = new Socket(dstAddress, dstPort);
+
+                PrintWriter out = new PrintWriter(new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream())),
+                        true);
+                out.println(lat);
+
+                ByteArrayOutputStream byteArrayOutputStream =
+                        new ByteArrayOutputStream(1024);
+                byte[] buffer = new byte[1024];
+
+                int bytesRead;
+                InputStream inputStream = socket.getInputStream();
+
+                /*
+                 * notice:
+                 * inputStream.read() will block if no data return
+                 */
+                while ((bytesRead = inputStream.read(buffer)) != -1){
+                    byteArrayOutputStream.write(buffer, 0, bytesRead);
+                    response += byteArrayOutputStream.toString("UTF-8");
+                }
+
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "UnknownHostException: " + e.toString();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "IOException: " + e.toString();
+            }finally{
+                if(socket != null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textResponse.setText(response);
+            super.onPostExecute(result);
+        }
+
+    }
+
+}
